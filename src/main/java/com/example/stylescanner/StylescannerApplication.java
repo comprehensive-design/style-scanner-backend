9c6657da2ed05790ce15fc5c42690aa8b4ec1559
HyeonjunOh <znzn2260@naver.com>
2024-04-04 13:29:24 +0900

    chore: first commit

---
+++ src/main/java/com/example/stylescanner/StylescannerApplication.java
@@ -0,0 +1,14 @@
+package com.example.stylescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class StylescannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylescannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stylescanner/StylescannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stylescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylescannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
