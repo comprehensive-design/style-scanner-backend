505c2b6db661199acf3f8866dc44173b108fea43
HyeonjunOh <znzn2260@naver.com>
2024-04-14 03:59:17 +0900

    fix: 엔티티 수정

--- src/main/java/com/example/stylescanner/feed_like/entity/Feed_Like.java
+++ src/main/java/com/example/stylescanner/feed_like/entity/Feed_Like.java
@@ -2,28 +2,37 @@ package com.example.stylescanner.feed_like.entity;
 
 import com.example.stylescanner.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
-import java.net.URL;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@Table(name = "feed_like")
 public class Feed_Like {
     @Id
-    @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer like_id;
+    private Long id;
 
     @Column(nullable = false)
-    private URL feed_url;
+    private String feedUrl;
 
     @CreatedDate
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
     @ManyToOne
-    private User user_id;
+    private User user;
+
+    @Builder
+    public Feed_Like(String feedUrl, User user) {
+        this.feedUrl = feedUrl;
+        this.user = user;
+    }
+
 }
--- src/main/java/com/example/stylescanner/item_like/entity/Item_Like.java
+++ src/main/java/com/example/stylescanner/item_like/entity/Item_Like.java
@@ -3,6 +3,7 @@ package com.example.stylescanner.item_like.entity;
 import com.example.stylescanner.item.entity.Item;
 import com.example.stylescanner.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,20 +15,24 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @NoArgsConstructor
-
+@Table(name = "item_like")
 public class Item_Like {
     @Id
-    @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer like_id;
+    private Long id;
 
     @CreatedDate
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
     @ManyToOne
-    private Item item_id;
+    private Item item;
 
     @ManyToOne
-    private User user_id;
+    private User user;
 
+    @Builder
+    public Item_Like(Item item, User user) {
+        this.item = item;
+        this.user = user;
+    }
 }
