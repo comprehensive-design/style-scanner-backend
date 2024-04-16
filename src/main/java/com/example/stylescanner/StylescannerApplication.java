5fd6d37d4ac410b66f753eb6ce0a93de8f165971
HyeonjunOh <znzn2260@naver.com>
2024-04-16 19:55:03 +0900

    feat: Swagger 초기 설정

--- src/main/java/com/example/stylescanner/StylescannerApplication.java
+++ src/main/java/com/example/stylescanner/StylescannerApplication.java
@@ -12,5 +12,4 @@ public class StylescannerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StylescannerApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/stylescanner/swagger/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.stylescanner.swagger.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(title = "StyleScanner WAS",
+                description = "StyleScanner API 명세",
+                version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi OpenApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("StyleScanner API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
