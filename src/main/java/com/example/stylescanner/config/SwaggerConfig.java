d4e29949520153acc70bc1768a27ab750fd1f34b
HyeonjunOh <znzn2260@naver.com>
2024-05-21 20:42:20 +0900

    feat: cors 관련 코드 추가

--- src/main/java/com/example/stylescanner/config/SwaggerConfig.java
+++ src/main/java/com/example/stylescanner/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.stylescanner.swagger.config;
+package com.example.stylescanner.config;
 
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
