099ac4a2f7039a51f6c52a83180702f82a24747a
daxxn <dahyeonhan4529@gmail.com>
2024-05-30 21:06:48 +0900

    feat: 커뮤니티,댓글 dto 추가

---
+++ src/main/java/com/example/stylescanner/comment/dto/CommentUpdateDto.java
@@ -0,0 +1,10 @@
+package com.example.stylescanner.comment.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CommentUpdateDto {
+    private String comment;
+}
--- src/test/java/com/example/stylescanner/StylescannerApplicationTests.java
+++ src/test/java/com/example/stylescanner/StylescannerApplicationTests.java
@@ -1,7 +1,13 @@
 package com.example.stylescanner;
 
+import com.example.stylescanner.instagram.util.InstagramGraphApiUtil;
+import org.json.JSONException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
 
 @SpringBootTest
 class StylescannerApplicationTests {
@@ -9,5 +15,13 @@ class StylescannerApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	@DisplayName("instagram graph api 연결 테스트")
+	void instagram_api() throws IOException, JSONException {
+		InstagramGraphApiUtil util = new InstagramGraphApiUtil();
+		util.SearchCeleb("you_r_love");
+	}
+
 
 }
