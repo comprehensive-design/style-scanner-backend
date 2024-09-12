bdde508287610b43c279440c0afad1835358e739
daxxn <dahyeonhan4529@gmail.com>
2024-09-12 19:21:56 +0900

    feat: /insta/home pagination 처리 + 하위 미디어 가져오는 API 개발

--- src/main/java/com/example/stylescanner/instagram/dto/HomeFeedResponseDto.java
+++ src/main/java/com/example/stylescanner/instagram/dto/HomeFeedResponseDto.java
@@ -2,9 +2,18 @@ package com.example.stylescanner.instagram.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+//@Data
+//public class HomeFeedResponseDto {
+//    List<FeedDto> feeds;
+//}
 
 @Data
 public class HomeFeedResponseDto {
-    List<FeedDto> feeds;
+    String username;
+    String profile_url;
+    String thumbnail_url;
+    String feed_code;
 }
--- src/main/java/com/example/stylescanner/instagram/service/InstagramService.java
+++ src/main/java/com/example/stylescanner/instagram/service/InstagramService.java
@@ -1,8 +1,10 @@
 package com.example.stylescanner.instagram.service;
 
 import com.example.stylescanner.follow.dto.FollowingListResponseDto;
+import com.example.stylescanner.follow.entity.Follow;
 import com.example.stylescanner.instagram.dto.*;
 import com.example.stylescanner.instagram.util.InstagramGraphApiUtil;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -34,28 +36,51 @@ public class InstagramService {
         return celebInstaResponseDto;
     }
 
-    public HomeFeedResponseDto readHomeFeed(FollowingListResponseDto followingList) {
-        HomeFeedResponseDto homeFeedResponseDto = new HomeFeedResponseDto();
+//    public HomeFeedResponseDto readHomeFeed(FollowingListResponseDto followingList) {
+//        HomeFeedResponseDto homeFeedResponseDto = new HomeFeedResponseDto();
+//
+//        List<FeedDto> AllFeedList = new ArrayList<>();
+//
+//        for(CelebProfileResponseDto celeb : followingList.getFollowing_list()){
+//            String username = celeb.getProfileName();
+//            try {
+//                List<FeedDto> feedList = instagramGraphApiUtil.GetRecentCelebFeed(username);
+//                AllFeedList.addAll(feedList);
+//            } catch (IOException e) {
+//                return null;
+//            }
+//        }
+//
+//        List<FeedDto> sort_AllFeedList =  AllFeedList.stream()
+//                .sorted(Comparator.comparing(FeedDto::getTimestamp).reversed())
+//                .collect(Collectors.toList());
+//
+//        homeFeedResponseDto.setFeeds(sort_AllFeedList);
+//
+//        return homeFeedResponseDto;
+//    }
 
-        List<FeedDto> AllFeedList = new ArrayList<>();
 
-        for(CelebProfileResponseDto celeb : followingList.getFollowing_list()){
-            String username = celeb.getProfileName();
-            try {
-                List<FeedDto> feedList = instagramGraphApiUtil.GetRecentCelebFeed(username);
-                AllFeedList.addAll(feedList);
-            } catch (IOException e) {
-                return null;
-            }
-        }
-
-        List<FeedDto> sort_AllFeedList =  AllFeedList.stream()
-                .sorted(Comparator.comparing(FeedDto::getTimestamp).reversed())
+    public  List<HomeFeedResponseDto> readHomeFeed(Page<Follow> paging) throws IOException {
+        List<String> followingList = paging.getContent().stream()
+                .map(Follow::getFolloweeId)
                 .collect(Collectors.toList());
 
-        homeFeedResponseDto.setFeeds(sort_AllFeedList);
+        List<HomeFeedResponseDto> responseDtos = new ArrayList<>();
+        for(String followeeId : followingList){
+            HomeFeedResponseDto dto = new HomeFeedResponseDto();
+            dto.setUsername(followeeId);
+            dto.setProfile_url(instagramGraphApiUtil.SearchCeleb(followeeId).getProfilePictureUrl());
 
-        return homeFeedResponseDto;
+            FeedDto feedDto = instagramGraphApiUtil.GetRecentCelebFeed_Rapid(followeeId);
+
+            dto.setThumbnail_url(feedDto.getMedia_url_list().get(0));
+            dto.setFeed_code(feedDto.getMedia_id());
+
+            responseDtos.add(dto);
+        }
+
+        return responseDtos;
     }
 
     public FeedDto readFeed(FeedRequestDto feedRequestDto) {
@@ -65,5 +90,9 @@ public class InstagramService {
         String beforeCursor = feedRequestDto.getBefore_cursor();
         String username = feedRequestDto.getUsername();
         return instagramGraphApiUtil.GetMedia(username,  mediaId, beforeCursor,feed_index);
+    }
+
+    public List<String> findCarousel(String feed_code) throws IOException {
+        return instagramGraphApiUtil.GetCarouselMedia(feed_code);
     }
 }
